b30e19203eeb714167ca12709e3c4afc02c7e9cd
Grzegorz Brzeziak <g-b3@wp.pl>
2021-06-14 10:32:56 +0200

    feat(app): AIO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {AboutComponent} from "./component/about/about/about.component";
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+import {PageNotFoundComponent} from "./component/page-not-found/page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  // {
+  //   path: 'posts',
+  //   component: PostsComponent
+  // },
+  // {
+  //   path: 'contact',
+  //   component: ContactComponent
+  // },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
